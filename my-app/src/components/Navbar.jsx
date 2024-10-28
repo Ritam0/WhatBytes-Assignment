a6f189540011c4f3ab88cfecf6cfe096c44eabf8
Ritam Majumder <ritmmajumder0@gmail.com>
2024-10-28 13:10:53 +0530

    nav items fixed

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 import Logo from '@/app/Assets/logo.png'
 const Navbar = () => {
   return (
-    <div className='flex w-screen  justify-center border-b-[2px]'>
+    <div className='flex w-screen  justify-center border-b-[2px] p-2'>
       <div className="logo  p-4 w-[200px]">
         <img src="https://res.cloudinary.com/djyxyaqno/image/upload/v1730026025/WB-logo_npgjyw.png" alt="logo" />
       </div>
-      <div className="profile  p-4 w-full text-right m-auto mr-4 border rounded-[5px] w-56 font-bold text-[20px]">
+      <div className="profile  p-4  text-right m-auto mr-4 border rounded-[5px] w-56 font-bold text-[20px]">
       🤦 Ritam Majumder
       </div>
     </div>
